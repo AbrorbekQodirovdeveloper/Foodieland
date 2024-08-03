a40c54aef8509204b05ea23eeab34ede5d6306f4
AbrorbekQodirovdeveloper <Abrorbekdev07@gmail.com>
2024-08-03 13:12:59 +0500

    foodiland

---
+++ src/NavFooter.js
@@ -0,0 +1,11 @@
+function MenuOpen () {
+    let menu = document.getElementById("menu");
+
+    menu.classList = "openMenu";
+}
+
+function MenuClose () {
+    let menu = document.getElementById("menu");
+
+    menu.classList = "menu";
+}
